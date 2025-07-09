3314b100f9812c85f98a25406c93b4326200af43
Андрій <gapeev1979@gmail.com>
2025-07-10 00:17:58 +0300

    test-3

--- src/js/events.js
+++ src/js/events.js
@@ -1,33 +1,34 @@
 import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
 const swiper = new Swiper('.events-swiper', {
-    slidesPerView: 1,
-    spaceBetween: 24,
-    loop: false,
-    autoHeight: false,
-    observer: true,
-    observeParents: true,
-    watchSlidesProgress: true,
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: false,
+  autoHeight: false,
+  observer: true,
+  observeParents: true,
+  watchSlidesProgress: true,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    1440: {
+      slidesPerView: 3,
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
-      },
-      1440: {
-        slidesPerView: 3,
-      }
-    }
+  },
 });
